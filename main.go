984fd9b981012a177c30fd2c07ce98c2b5cfd623
Ning Hu <ning.hu1205@gmail.com>
2019-08-18 18:47:56 -0700

    getting ssl cert

--- main.go
+++ main.go
@@ -7,34 +7,47 @@ import (
 	"os"
 
 	"github.com/ning-hu/mh-line-bot/linebot"
-	"github.com/ning-hu/mh-line-bot/linebot/httphandler"
 )
 
 func main() {
 	secret := os.Getenv("LINE_SECRET")
 	accessToken := os.Getenv("LINE_ACCESS_TOKEN")
 
-	handler, err := httphandler.New(secret, accessToken)
+	bot, err := linebot.New(secret, accessToken)
 	if err != nil {
 		log.Fatal("Error creating a new http handler")
 	}
 
-	handlerFunc := func(events []*linebot.Event, r *http.Request) {
-		bot, err := handler.NewClient()
+	// Setup HTTP Server for receiving requests from LINE platform
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
 		if err != nil {
-			log.Fatal(err)
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
+			return
 		}
-
-		fmt.Printf("%+v", events)
-
 		for _, event := range events {
 			if event.Type == linebot.EventTypeMessage {
-				_, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("hello")).Do()
-				if err != nil {
-					fmt.Printf("%v\n", err)
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
 				}
 			}
 		}
+	})
+
+	http.HandleFunc("/.well-known/acme-challenge/iejmNt4sz-ZKxe9H0ExzIT8hguHWtbhgBQL9UeLSxA8", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "iejmNt4sz-ZKxe9H0ExzIT8hguHWtbhgBQL9UeLSxA8.PXkXecLYabBTs7tPtwiUDttGgWLCXL3AGQljK8RLd84")
+	})
+
+	// This is just sample code.
+	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
 	}
-	handler.HandleEvents(handlerFunc)
 }
